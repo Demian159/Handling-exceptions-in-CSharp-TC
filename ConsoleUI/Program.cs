344d5e10a8dc1c5ac615bd5de7e6464c0553b407
Demian159 <adrian_salatino@hotmail.com>
2021-08-02 17:57:23 -0300

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using ExceptionsLibrary;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoCode demo = new DemoCode();
+            try
+            {
+                int result = demo.GrandParentMethod(4);
+                Console.WriteLine($"The value at the given position is { result }");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ypu give us bad information. Bad user!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                var inner = ex.InnerException;
+                while (inner != null)
+                {
+                    Console.WriteLine(inner.StackTrace);
+                    inner = inner.InnerException;
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
